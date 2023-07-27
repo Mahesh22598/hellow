c7496bbaada5c415a8b0193e3bcbf619e3a6b753
mahesh <140531490+Mahesh22598@users.noreply.github.com>
2023-07-27 23:26:37 +0530

    Update hellow.java

--- hellow.java
+++ hellow.java
@@ -2,7 +2,7 @@ public class hellow {
 public static void main(String[] args) {
 // TODO Auto-generated method stub
 System.out.println("Hello Agni");
-System.out.println("Hello Riya");
+System.out.println("Hello Riyhbbvva");
   System.out.println("Hello mahesh");
    System.out.println("Hello king22");
    System.out.println("Hello kingini");
