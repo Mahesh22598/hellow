87700c53063a96506c739208b18a53a6bfbb8695
mahesh <140531490+Mahesh22598@users.noreply.github.com>
2023-07-27 22:27:30 +0530

    Update hellow.java

--- hellow.java
+++ hellow.java
@@ -4,5 +4,6 @@ public static void main(String[] args) {
 System.out.println("Hello Agni");
 System.out.println("Hello Riya");
   System.out.println("Hello mahesh");
+   System.out.println("Hello king22");
   
 }
