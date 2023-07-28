d4ac168a7d0a0cf0b931b6b1ecabadff8604459a
mahesh <140531490+Mahesh22598@users.noreply.github.com>
2023-07-28 11:22:07 +0530

    Update hellow.java

--- hellow.java
+++ hellow.java
@@ -6,7 +6,7 @@ System.out.println("Hello Riyhbbvva");
   System.out.println("Hello mahesh");
    System.out.println("Hello king22");
    System.out.println("Hello hi kingini");
-  System.out.println("Helloohhhoo mahhhi11");  
+  System.out.println("Hello mahhhi11");  
 
 }
 }
